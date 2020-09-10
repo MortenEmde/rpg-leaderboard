2b270be1b64e511da5f7575997566d88997a7118
Emde <mortenemdec@gmail.com>
2020-09-10 14:17:26 +0200

    add sign-in page

--- app.js
+++ app.js
@@ -31,6 +31,12 @@ app.use(cookieParser());
 // require passport auth
 require('./auth/auth');
 
+app.use(express.static(__dirname + '/public'));
+ 
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
 // main routes
 app.use('/', routes);
 app.use('/', passport.authenticate('jwt', { session : false }), secureRoutes);
